18eb88e010f9335896e6a7ee8cc504d18444c16d
jakubkrup99 <118464265+jakubkrup99@users.noreply.github.com>
2024-11-28 22:05:24 +0100

    Add project files.

---
+++ Table/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Table.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
